ff77061602760d3b83ab63b7f604cdea9222a993
keiko <keiko@192.168.10.104>
2024-08-31 17:46:27 +0900

    お疲れ様です。 課題java基礎1.3-1java導入完了しましたのでご確認お願い致します。

---
+++ HelloWorld_TakedaKeiko/src/sample/SelfIntroduction.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//氏名
+		String name = "竹田 桂子";
+		//年齢
+		int age = 27;
+		//趣味
+		String hobby = "料理";
+		/*自己紹介文を出力する処理*/
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
